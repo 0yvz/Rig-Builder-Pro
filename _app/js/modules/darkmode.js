9488710226ad83a068f90fe08ba48581b64f9ff8
0yvz <oyvindg@live.no>
2023-04-01 19:47:38 +0200

    Add darkmode option

---
+++ _app/js/modules/darkmode.js
@@ -0,0 +1,13 @@
+export default function toggleDarkMode() {
+	console.log('Dark mode toggle button clicked');
+	const root = document.documentElement;
+	const button = document.querySelector('.header__toggle-button');
+	if (root.classList.contains('dark-mode')) {
+	  root.classList.remove('dark-mode');
+	  button.textContent = 'Dark Mode';
+	} else {
+	  root.classList.add('dark-mode');
+	  button.textContent = 'Light Mode';
+	}
+ }
+ 

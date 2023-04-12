23983fc0d80896d863c1d92db5b8975f6b9ada80
0yvz <oyvindg@live.no>
2023-04-12 11:31:48 +0200

    add components and settings tab

--- _studio/sanity.config.js
+++ _studio/sanity.config.js
@@ -2,6 +2,8 @@ import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 
 import schemas from './schemas/schemas.js';
+import components from './structure/components.js';
+import settings from './structure/settings.js';
 
 export default {
 	title: 'PC builds',
@@ -9,7 +11,21 @@ export default {
 	projectId: 'cizkibmh',
 	dataset: 'components',
 
-	plugins: [deskTool(), visionTool()],
+	plugins: [
+		deskTool({
+			title: 'Components',
+			name: 'components',
+			structure: components
+		}),
+
+		deskTool({
+			title: 'Settings',
+			name: 'settings',
+			structure: settings
+		}),
+
+		visionTool()
+	],
 
 	schema: {
 		types: schemas,

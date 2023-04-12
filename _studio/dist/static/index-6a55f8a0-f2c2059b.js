940c458cb33146a6d24319fd32995bf8f25440d5
0yvz <oyvindg@live.no>
2023-04-12 15:07:59 +0200

    updated sanity

---
+++ _studio/dist/static/index-6a55f8a0-f2c2059b.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-2cbd8830-60db4feb.js";import"./sanity-0c1220b8.js";import"./index-688bd4df.js";export{m as DocumentPaneProvider,a as default};

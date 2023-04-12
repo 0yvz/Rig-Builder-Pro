64093893afa3144be59c08943cc8523eabc11740
0yvz <oyvindg@live.no>
2023-04-12 11:29:58 +0200

    quick fix

--- _app/js/modules/darkmode.js
+++ _app/js/modules/darkmode.js
@@ -1,7 +1,9 @@
 export default function toggleDarkMode() {
 	console.log('Dark mode toggle button clicked');
+	
 	const root = document.documentElement;
 	const button = document.querySelector('.header__toggle-button');
+
 	if (root.classList.contains('dark-mode')) {
 	  root.classList.remove('dark-mode');
 	  button.textContent = 'Dark Mode';

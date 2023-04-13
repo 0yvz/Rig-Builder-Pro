efa1dd947643fe7e8c82bddf0461af1f331def3d
0yvz <oyvindg@live.no>
2023-04-14 01:48:24 +0200

    import header toggle

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,9 @@
 import toggleDarkMode from './modules/darkmode.js';
+import toggleHeaderMenu from './modules/header.js';
 
 // Call the toggleDarkMode function when the button is clicked
 const button = document.querySelector('.header__toggle-button');
 button.addEventListener('click', toggleDarkMode);
+
+// Call the toggleHeaderMenu function when the logo is clicked
+toggleHeaderMenu();

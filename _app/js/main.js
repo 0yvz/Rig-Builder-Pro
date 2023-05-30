0928d5aec5440315fa6bd03a839cdaf6c506a39b
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-30 17:29:12 +0200

    Imported fetchdata.js in main

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,6 @@
 import toggleDarkMode from './modules/darkmode.js';
 import toggleHeaderMenu from './modules/header.js';
+import FetchData from './modules/fetchData.js';
 
 // Call the toggleDarkMode function when the button is clicked
 const button = document.querySelector('.header__toggle-button');
@@ -7,3 +8,4 @@ button.addEventListener('click', toggleDarkMode);
 
 // Call the toggleHeaderMenu function when the logo is clicked
 toggleHeaderMenu();
+FetchData ();

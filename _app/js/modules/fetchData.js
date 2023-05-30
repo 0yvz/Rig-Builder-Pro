aac7eb1d8159612a365e4a67d1494d17c98fd536
madsemilf <55183287+Madsemilf@users.noreply.github.com>
2023-05-30 17:48:33 +0200

    Adjusted fetchdata.js

--- _app/js/modules/fetchData.js
+++ _app/js/modules/fetchData.js
@@ -2,12 +2,16 @@ import {sanity} from '../sanity.js';
 
 export default async function FetchData() {
 	const productsContainer = document.querySelector('.products');
-	const query = `*[_type == 'product'] {
-		...,
-		preview {
-			..., asset->
+	const query = `*[_type == 'cpu'] {
+		_id,
+		name,
+		image {
+		  asset->{
+			 _id,
+			 url
+		  }
 		}
-	}`;
+	 }`;
 	const products = await sanity.fetch(query);
 	console.log(products);
 }
